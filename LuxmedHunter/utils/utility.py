be86252cfefed630f6465d0ace8b7ee6a61a4b4e
Kalindro <25466220+Kalindro@users.noreply.github.com>
2023-06-11 17:58:09 +0200

    fixes

--- LuxmedHunter/utils/utility.py
+++ LuxmedHunter/utils/utility.py
@@ -2,6 +2,7 @@ import datetime as dt
 
 import pandas as pd
 import requests
+from requests.exceptions import JSONDecodeError
 
 from LuxmedHunter.utils.logger_custom import default_logger as logger
 
@@ -14,11 +15,11 @@ def handle_response(response: requests.Response):
     response.raise_for_status()
     try:
         return response.json()
-    except Exception as err:
-        logger.error(f"Status code: {response.status_code}")
-        logger.error(f"Response text: {response.text}")
-        logger.error(f"Response: {response}")
+    except JSONDecodeError as err:
         logger.exception(f"Error on json encoding: {err}")
+        logger.error(f"Status code: {response.status_code}")
+        logger.error(f"Response: {response}")
+        return None
 
 
 def date_string_to_datetime(date_string: str) -> dt.datetime:
